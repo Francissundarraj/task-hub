7e666cbfd48d9909fedb84036fd9a536be29cf14
francissundarraj02@gmail.com <francissundarraj02@gmail.com>
2025-02-04 00:49:55 +0530

    Completed the project

---
+++ client/tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      cursor: {
+        c1: 'url(./assets/c1.png), pointer',
+        c2: 'url(./assets/c2.png), pointer',
+        c3: 'url(./assets/c3.png), pointer',
+        c4: 'url(./assets/c4.png), pointer',
+        c5: 'url(./assets/c5.png), pointer',   
+        c6: 'url(./assets/c6.png), pointer',   
+        c7: 'url(./assets/c7.png), pointer', 
+        c8: 'url(./assets/c8.png), pointer',
+        c9: 'url(./assets/c9.png), pointer',   
+        c10: 'url(./assets/c10.png), auto',
+        c11: 'url(./assets/c11.png), pointer',
+        c12: 'url(./assets/c12.png), pointer',
+        c13: 'url(./assets/c13.png), pointer',
+        c14: 'url(./assets/c14.png), pointer',
+        c15: 'url(./assets/c15.png), pointer',
+        c16: 'url(./assets/c16.png), pointer',
+        c17: 'url(./assets/c17.png), pointer',    
+        c18: 'url(./assets/c18.png), pointer',
+        c19: 'url(./assets/c19.png), pointer',
+        c20: 'url(./assets/c20.png), pointer',
+        c21: 'url(./assets/c21.png), pointer',
+        finger: 'url(./assets/finger.png), pointer',
+        finger1: 'url(./assets/finger1.png), pointer',
+        finger2: 'url(./assets/finger2.png), pointer',
+        finger3: 'url(./assets/finger3.png), pointer',
+        finger4: 'url(./assets/finger4.png), pointer',
+        finger5: 'url(./assets/finger5.png), pointer',
+        finger6: 'url(./assets/finger6.png), pointer',
+        finger7: 'url(./assets/finger7.png), pointer',
+        finger8: 'url(./assets/finger8.png), pointer',
+        
+      },
+
+    },
+  },
+  plugins: [],
+}
+
