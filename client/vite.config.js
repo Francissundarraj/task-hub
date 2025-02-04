55d68de4318f27e4abb2f7cc4af7f7ffc8b22c58
francissundarraj02@gmail.com <francissundarraj02@gmail.com>
2025-02-04 16:06:53 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Optional: Specify the port for the development server
+    open: true, // Optional: Automatically open the app in the browser
+  },
+  build: {
+    outDir: 'dist', // Optional: Specify the output directory for the build
+  },
   
 })
